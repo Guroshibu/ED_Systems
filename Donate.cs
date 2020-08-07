1947dd57bd4a8928f06964eb852e51dc0be75c44
Guroshibu <guros@DESKTOP-2MUGCPP>
2020-08-07 08:11:04 +0300

    Donate change

--- Donate.cs
+++ Donate.cs
@@ -8,13 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using Yandex.Money.Api.Sdk;
-using Yandex.Money.Api.Sdk.Authorization;
-using Yandex.Money.Api.Sdk.Interfaces;
-using Yandex.Money.Api.Sdk.Net;
-using Yandex.Money.Api.Sdk.Requests;
-using Yandex.Money.Api.Sdk.Responses;
-using Yandex.Money.Api.Sdk.Utils;
+using System.Net;
 
 using Newtonsoft.Json;
 
@@ -25,10 +19,6 @@ namespace ED_Systems
         private string client_id;
         private string instance_id;
         private string redirect_uri = "https://yandex.ru/";
-
-        private static IHostProvider hp = new DefaultHostsProvider();
-        private static Authenticator auth = new Authenticator();
-        IHttpClient dhpc = new DefaultHttpPostClient(hp, auth);
 
         public FormDonate()
         {
@@ -42,31 +32,11 @@ namespace ED_Systems
 
         }
 
-        private async Task<string> GetInstanceID()
+        private void GetInstanceID()
         {
-            TaskCompletionSource<string> tcs;
-            tcs = new TaskCompletionSource<string>();
-
-            var instanceIdRequest = new InstanceIdRequest<InstanceIdResult>(dhpc, new JsonSerializer<InstanceIdResult>())
-            {
-                ClientId = client_id
-            };
-
-            var instanceIdResult = await instanceIdRequest.Perform();
-
-            if (instanceIdResult.Status == ResponseStatus.Success)
-            {
-                instance_id = instanceIdResult.InstanceId;
-                Properties.Settings.Default.instance_id = instance_id;
-                Properties.Settings.Default.Save();
-            }
-
-            return tcs.Task.Result;
+           
         }
     }
 
-    public class Authenticator : DefaultAuthenticator
-    {
-        public override string Token { get; set; }
-    }
+    
 }
